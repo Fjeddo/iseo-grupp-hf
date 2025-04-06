9c9f95b8159400956c01618ad0c6425cb17e39b2
F J <f@j.xyz>
2025-04-06 11:22:44 +0200

    Use mapgroup for base route

--- src/RivaRental.Web/Program.cs
+++ src/RivaRental.Web/Program.cs
@@ -1,7 +1,11 @@
 using RivaRental.Domain;
 using RivaRental.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = "rest/api"
+});
 
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<PriceCalculator>();
@@ -16,7 +20,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/rent", (RentRequest rentRequest, RentalService service, HttpContext ctx) =>
+var restApi = app.MapGroup("rest/api");
+
+restApi.MapPost("/rent", (RentRequest rentRequest, RentalService service, HttpContext ctx) =>
 {
     var (additional, boat) = service.TryBook(rentRequest.BoatType);
 
@@ -25,15 +31,36 @@ app.MapPost("/rent", (RentRequest rentRequest, RentalService service, HttpContex
         ctx.Response.Headers.Append("extra-possible", "true");
     }
     
-    return Results.AcceptedAtRoute("Booking", new Booking
+    return Results.AcceptedAtRoute("Booking", new
         {
             BookingNumber = $"{DateTimeOffset.UtcNow.UtcTicks}_{boat.GetType().Name}",
         });
 });
 
-app.MapGet("/booking", () => "Extra!").WithName("Booking");
+restApi.MapGet("/booking", (string bookingNumber, HttpContext ctx) =>
+{
+    if (ctx.Request.Headers.Accept.Any(s => s.Contains("image") && s.Contains("png")))
+    {
+        if (bookingNumber == "1")
+        {
+            return Results.File(File.ReadAllBytes("static/diable.png"), contentType: "image/png");
+        }
 
-app.MapPost("/return", (ReturnRequest returnRequest, RentalService service) => "Return!");
+        if (bookingNumber == "2")
+        {
+            return Results.File(File.ReadAllBytes("static/dolceriva.png"), contentType: "image/png");
+        }
+
+        if (bookingNumber == "3")
+        {
+            return Results.File(File.ReadAllBytes("static/iseosuper.png"), contentType: "image/png");
+        }
+    }
+
+    return Results.Ok("Extra!");
+}).WithName("Booking");
+
+restApi.MapPost("/return", (ReturnRequest returnRequest, RentalService service) => "Return!");
 
 app.Run();
 
